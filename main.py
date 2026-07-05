1e4420c0fb3306f9b02e3c03ac3edc60da76ba7d
Jakub JZ <jakub.jzbrozek@gmail.com>
2026-07-05 02:00:32 +0200

    Update weather alert logic and message formatting
    Refactor weather alert messaging and add detailed output.

--- main.py
+++ main.py
@@ -20,23 +20,25 @@ response.raise_for_status()
 
 data=response.json()
 will_rain = False
-list_of_weather = []
+list_of_weather1 = []
+list_of_weather2 = []
 for i in range(weather_params["cnt"]):
+    exact_hour = data["list"][i]["dt_txt"].split(" ")[1].split(":")[0]
+    list_of_weather1.append(data["list"][i]["weather"][0]["description"])
+    list_of_weather2.append(f"{exact_hour}-{int(exact_hour)+3}")
     if data["list"][i]["weather"][0]["id"] < 700:
-        list_of_weather.append(data["list"][i]["weather"][0]["main"])
         will_rain=True
 if will_rain:
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        from_ = "+12185100579",
-        body=f'WEATHER ALERT - {set(list_of_weather)}',
-        to='+48572494900'
-    )
+    body1='Warning! - WEATHER ALERT: \n'
 else:
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        from_ = "+12185100579",
-        body='NO RAIN TODAY',
-        to='+48572494900'
-    )
+    body1='WEATHER ALERT: \n'
+for i in range(weather_params["cnt"]):
+    body1 += f"{list_of_weather1[i]}: {list_of_weather2[i]}\n"
+print(body1)
+client = Client(account_sid, auth_token)
+message = client.messages.create(
+    from_ = "+12185100579",
+    body=body1,
+    to='+48572494900'
+)
 print(message.status)
